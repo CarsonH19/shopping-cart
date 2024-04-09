bbebaf64db1b22a6dafd0a30be5a502b0eafe052
CarsonH19 <questline.carson@gmail.com>
2024-04-09 23:51:57 +0900

    feat: Greeting component

--- src/components/Home/Greeting.jsx
+++ src/components/Home/Greeting.jsx
@@ -1,11 +1,11 @@
-// import classes from "./Greeting.module.css";
+import classes from "./Greeting.module.css";
 
 export default function Greeting() {
   return (
-    <section>
-      <h1>Title</h1>
-      <p>Text</p>
-      <button>Shop</button>
+    <section className={classes.greeting}>
+      <h1>Your one-stop destination for all your shopping needs</h1>
+      <p>Discover an unparalleled shopping experience with our extensive selection of products, unbeatable prices, and exceptional customer service. Shop now and transform your shopping journey with us.</p>
+      <button>Shop now</button>
     </section>
   );
 }
