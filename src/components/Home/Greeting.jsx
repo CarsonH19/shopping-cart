922efecfebc189b093bc4418a44325ea267015b3
CarsonH19 <questline.carson@gmail.com>
2024-04-12 00:19:46 +0900

    feat: shop now button logic

--- src/components/Home/Greeting.jsx
+++ src/components/Home/Greeting.jsx
@@ -1,11 +1,18 @@
 import classes from "./Greeting.module.css";
+import { useNavigate } from "react-router-dom";
 
 export default function Greeting() {
+  const navigate = useNavigate();
+
+  const handleShopNow = () => {
+    return navigate('/store');
+  }
+
   return (
     <section className={classes.greeting}>
       <h1>Your one-stop destination for all your shopping needs</h1>
       <p>Discover an unparalleled shopping experience with our extensive selection of products, unbeatable prices, and exceptional customer service. Shop now and transform your shopping journey with us.</p>
-      <button>Shop now</button>
+      <button onClick={handleShopNow}>Shop now</button>
     </section>
   );
 }
