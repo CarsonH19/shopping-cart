dd51dd9996fca730fa42913849e8e08f35c0fbe7
CarsonH19 <questline.carson@gmail.com>
2024-04-12 00:02:50 +0900

    feat: cart icon

--- src/components/MainNavigation/MainNavigation.jsx
+++ src/components/MainNavigation/MainNavigation.jsx
@@ -1,38 +1,45 @@
 import classes from "./MainNavigation.module.css";
 import { NavLink } from "react-router-dom";
 import SearchBar from "../UI/SearchBar";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
 
 export default function MainNavigation() {
   return (
     <header className={classes.header}>
       <h1>Store Name</h1>
       <SearchBar />
-      <nav>
-        <ul className={classes.list}>
-          <li>
-            <NavLink
-              to="/"
-              className={({ isActive }) =>
-                isActive ? classes.active : undefined
-              }
-              end
-            >
-              Home
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              to="/store"
-              className={({ isActive }) =>
-                isActive ? classes.active : undefined
-              }
-            >
-              Store
-            </NavLink>
-          </li>
-          <li>CART ICON</li>
-        </ul>
-      </nav>
+      <div className={classes['nav-links']}>
+        <nav>
+          <ul className={classes.list}>
+            <li>
+              <NavLink
+                to="/"
+                className={({ isActive }) =>
+                  isActive ? classes.active : undefined
+                }
+                end
+              >
+                Home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/store"
+                className={({ isActive }) =>
+                  isActive ? classes.active : undefined
+                }
+              >
+                Store
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+        <div className={classes['cart-box']}>
+          <FontAwesomeIcon icon={faCartShopping} fontSize="26px" cursor="pointer" />
+          <span className={classes['cart-quantity']}>0</span>
+        </div>
+      </div>
     </header>
   );
 }
