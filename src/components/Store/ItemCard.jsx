a715c670d9f93e8b9f0f7522382b817f24be0b5d
CarsonH19 <questline.carson@gmail.com>
2024-04-12 00:19:24 +0900

    feat: Link component

--- src/components/Store/ItemCard.jsx
+++ src/components/Store/ItemCard.jsx
@@ -1,18 +1,21 @@
-import classes from "./ItemCard.module.css"
+import classes from "./ItemCard.module.css";
 import { currencyFormatter } from "../../util/formatting";
+import { Link } from "react-router-dom";
 
 export default function ItemCard({ item }) {
   return (
     <li className={classes.item}>
-      <article className={classes['item-article']}>
-        <img src={item.image} alt={item.title} />
-        <div className={classes['item-card-info']}>
-          <h3>{item.title}</h3>
-          <p className={classes['item-price']}>
-            {currencyFormatter.format(item.price)}
-          </p>
-        </div>
-      </article>
+      <Link to={`/store/${item.id}`}>
+        <article className={classes["item-article"]}>
+          <img src={item.image} alt={item.title} />
+          <div className={classes["item-card-info"]}>
+            <h3>{item.title}</h3>
+            <p className={classes["item-price"]}>
+              {currencyFormatter.format(item.price)}
+            </p>
+          </div>
+        </article>
+      </Link>
     </li>
   );
 }
