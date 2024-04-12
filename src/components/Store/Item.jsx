912f3c782b9589ff29b68cbf0947e477c428ca5b
CarsonH19 <questline.carson@gmail.com>
2024-04-12 22:03:24 +0900

    style: Item component

--- src/components/Store/Item.jsx
+++ src/components/Store/Item.jsx
@@ -1,27 +1,32 @@
-// import classes from "./Item.module.css";
-
-import { json } from 'react-router-dom';
+import classes from "./Item.module.css";
 
 export default function Item({ item }) {
   console.log(item);
 
   return (
-    <section>
-       <div>
+    <section className={classes["item-container"]}>
+      <div className={classes.item}>
         <img src={item.image} alt={item.title} />
-        <div>
+        <div className={classes["item-info"]}>
           <h1>{item.title}</h1>
-          <p>{item.price}</p>
-          <input type="number" name="quantity" id="quantity" />
+          <p className={classes.price}>${item.price.toFixed(2)}</p>
+          <div className={classes.quantity}>
+            <label htmlFor="number">Quantity:</label>
+            <input
+              type="number"
+              name="quantity"
+              id="quantity"
+              placeholder="1"
+            />
+          </div>
           <button>Add to Cart</button>
         </div>
-       </div>
-       <hr />
-       <div>
+      </div>
+      <hr />
+      <div className={classes["item-details"]}>
         <h2>Description</h2>
         <p>{item.description}</p>
-       </div>
+      </div>
     </section>
-  )
+  );
 }
-
