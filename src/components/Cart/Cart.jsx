03c26c0ff5241629a1add91297285e6d8353c916
CarsonH19 <questline.carson@gmail.com>
2024-04-15 00:50:06 +0900

    fix: cart item spacing

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,32 +1,34 @@
 import classes from "./Cart.module.css";
 import Modal from "../UI/Modal";
 import { useSelector, useDispatch } from "react-redux";
-import { uiActions } from '../../store/ui-slice'
+import { uiActions } from "../../store/ui-slice";
 import CartItem from "./CartItem";
 
 export default function Cart() {
   const dispatch = useDispatch();
   const cartItems = useSelector((state) => state.cart.items);
   const numberOfItems = useSelector((state) => state.cart.totalQuantity);
-  
+
   const totalPrice = cartItems.reduce((total, currentItem) => {
-    return total + (currentItem.price * currentItem.quantity);
+    return total + currentItem.price * currentItem.quantity;
   }, 0);
 
   const handleCloseModal = () => {
     dispatch(uiActions.toggle());
-  }
-
-
+  };
 
   return (
     <Modal className={classes.cart}>
-      <p className={classes.close} onClick={handleCloseModal}>X</p>
+      <p className={classes.close} onClick={handleCloseModal}>
+        X
+      </p>
 
-      <h1>Cart ({numberOfItems})</h1>
-      { cartItems.map((item) => (
-        <CartItem  key={item.id} item={item}/>
-      ))}
+      <div>
+        <h1>Cart ({numberOfItems})</h1>
+        {cartItems.map((item) => (
+          <CartItem key={item.id} item={item} />
+        ))}
+      </div>
 
       <div className={classes.checkout}>
         <button className={classes.button}>Checkout</button>
